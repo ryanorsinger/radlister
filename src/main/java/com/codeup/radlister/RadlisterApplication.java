047855b957dbc627be66a80ce5a2a02ed4b9d29d
Ryan Orsinger <ryan.orsinger@gmail.com>
2018-04-16 11:16:49 -0500

    Initial commit

---
+++ src/main/java/com/codeup/radlister/RadlisterApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.radlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadlisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadlisterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codeup/radlister/RadlisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.radlister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RadlisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
